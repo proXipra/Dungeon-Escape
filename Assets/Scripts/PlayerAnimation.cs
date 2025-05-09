cabacbc5f4cd046774d225ce168f46859869e6ca
Cihan <cihanbalkr@gmail.com>
2025-05-09 18:14:47 +0300

    Player Animations

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _anim;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _anim = GetComponentInChildren<Animator>();
+    }
+
+    public void Move(float move)
+    {
+        _anim.SetFloat("Move", Mathf.Abs(move));
+    }
+
+    public void UpdateBool(string name, bool value)
+    {
+        _anim.SetBool(name, value);
+    }
+}
