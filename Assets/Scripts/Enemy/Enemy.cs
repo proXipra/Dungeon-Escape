f1d6058f35f89f98008263bc2f61ab2fcf43c708
Cihan <cihanbalkr@gmail.com>
2025-05-17 01:41:04 +0300

    Best Practice Inheritance
    Shared behaviors implemented into Enemy class.

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,15 +1,60 @@
+using System;
+using System.Linq;
 using UnityEngine;
 
 public abstract class Enemy : MonoBehaviour
 {
-    protected int _health;
+    [SerializeField] protected int _health;
     [SerializeField] protected int _speed;
-    protected int _gem;
+    [SerializeField] protected int _gem;
 
-    protected virtual void Attack()
+    [SerializeField] protected Transform waypointsParent;
+    protected Transform[] waypoints;
+    protected Vector3 _targetPos;
+    protected Animator _animator;
+    protected SpriteRenderer _sprite;
+
+    protected virtual void Init()
     {
+        waypoints = waypointsParent != null ? waypointsParent.GetComponentsInChildren<Transform>().
+            Where(i => i != waypointsParent).ToArray() : throw new Exception("Waypoint Parent is null!");
 
+        _animator = GetComponentInChildren<Animator>() ??
+            throw new MissingComponentException("Animator is NULL!");
+        _sprite = GetComponentInChildren<SpriteRenderer>() ??
+            throw new MissingComponentException("SpriteRenderer is NULL");
     }
 
-    protected abstract void Update();
+    private void Start()
+    {
+        Init();
+    }
+
+    protected virtual void Update()
+    {
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsTag("Idle"))
+        {
+            return;
+        }
+
+        Movement();
+    }
+
+    protected virtual void Movement()
+    {
+        _sprite.flipX = _targetPos == waypoints[0].position ? true : false;
+
+        if (transform.position.x == waypoints[0].position.x)
+        {
+            _targetPos = waypoints[1].position;
+            _animator.SetTrigger("Idle");
+        }
+        else if (transform.position.x == waypoints[1].position.x)
+        {
+            _targetPos = waypoints[0].position;
+            _animator.SetTrigger("Idle");
+        }
+        transform.position = Vector3.MoveTowards(transform.position, _targetPos, _speed * Time.deltaTime);
+    }
+
 }
---
+++ Assets/Scripts/Enemy/Spider.cs
@@ -0,0 +1,5 @@
+
+public class Spider : Enemy
+{
+
+}
